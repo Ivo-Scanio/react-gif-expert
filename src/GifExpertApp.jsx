ca125f3eb8f75b4f8a472eea07f99d02b53f4e57
ivo scanio <ivo.e@hotmail.es>
2023-04-25 22:30:05 -0300

    Update GifExpertApp.jsx

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,7 @@ import { AddCategory,GifGrid } from "./components";
 export const GifExpertApp = () => {
     const apikey='cZErPcq68ryGl4Q5cogVIip4Gu5siqEr';
   
-    const [categories, setCategories]= useState(['marin kitagawa']);
+    const [categories, setCategories]= useState(['F1']);
 
     const onAddCategory=(newCategory)=>{
      if(categories.includes(newCategory))return;
